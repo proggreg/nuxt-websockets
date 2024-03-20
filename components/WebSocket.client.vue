b2f0e6b4d1a355aa37b5d2c138c8f1fa73767e25
Greg Field <greg.field1992@gmail.com>
2024-03-20 22:34:46 +0000

    add username and validation

--- components/WebSocket.client.vue
+++ components/WebSocket.client.vue
@@ -4,22 +4,40 @@ const { status, data, send, open, close } = useWebSocket(`ws://${location.host}/
 
 const history = ref<string[]>([])
 watch(data, (newValue) => {
-  history.value.push(`server: ${newValue}`)
+  console.log('watch data', newValue)
+  history.value.push(`${newValue}`)
 })
 
 const message = ref('')
+const userName = ref('')
 function sendData(){
-  history.value.push(`client: ${message.value}`)
+  history.value.push(`${userName.value}: ${message.value}`)
   send(message.value)
   message.value = ''
+}
+
+const edit = ref(true)
+
+function enterUsername(){
+  send(JSON.stringify({
+    type: 'username',
+    value: userName.value
+  }))
+  edit.value = false
 }
 </script>
 
 <template>
-  <h1>WebSocket - let's go!</h1>
+  <h1>WebSocket - let's go! {{ status }}</h1>
+  <form @submit.prevent="enterUsername">
+    <Input :disabled="!edit" v-model="userName"/>
+    <Button v-if="edit" type="submit">Save Username</Button>
+    <Button v-else @click="edit = true">Edit Username</Button>
+  </form>
+
   <form @submit.prevent="sendData">
     <Input v-model="message"/>
-    <Button type="submit">Send</Button>
+    <Button :disabled="!userName || edit" type="submit">Send</Button>
   </form>
   <Card>
     <CardHeader>
--- server/api/websocket.ts
+++ server/api/websocket.ts
@@ -1,13 +1,16 @@
 import type { Peer, Message } from 'crossws'
 
 const room = 'ROOM'
+
+const userNames = new Map<Peer, string>()
+
 export default defineWebSocketHandler({
-  open(peer) {
+  open(peer: Peer) {
     console.log('opened WS', peer)
     peer.subscribe(room)
     peer.publish(room, 'Another user joined the chat')
   },
-  close(peer) {
+  close(peer: Peer) {
     console.log('closed WS', peer)
   },
   error(peer, error) {
@@ -15,8 +18,19 @@ export default defineWebSocketHandler({
   },
   message(peer, message) {
     console.log('message on WS', peer, message)
+    const data = message.text()
+    // console.log('data', typeof data, data)
+    if (data.includes('"type":"username"')) {
+      let userName = JSON.parse(message.text())
+      userNames.set(peer.id, userName.value)
+      console.log('username', userName.value)
+    } else {
+      peer.publish(room, `${userNames.get(peer.id)}: ${data}`)
+    }
 
-    peer.publish(room, message.text())
+  },
+  resolve(req) {
+    console.log('resolve', req)
   }
 })
 
