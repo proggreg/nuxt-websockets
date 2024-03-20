ada5c870cddd8e2d95124a98e102400bbae286b5
Greg Field <greg.field1992@gmail.com>
2024-03-20 19:56:15 +0000

    Add WebSocket component and configure Nuxt for WebSocket support

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <WebSocket />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@vueuse/nuxt'],
+  nitro: {
+    experimental: {
+      websocket: true
+    }
+  },
   devtools: { enabled: true }
 })
